b0c4050049b018b0a5517ff8c7d99b5d3602ce37
Julius <julkoske@gmail.com>
2021-05-03 03:09:43 +0300

    //j

--- graph/src/graph_add_node.c
+++ graph/src/graph_add_node.c
@@ -1,10 +1,12 @@
 /*******************************************************************************
- * Add a node to n to graph g. New adjacency lists in and out are created for
- * the node, after which the node is added into the nodes list in the graph.
+ * \brief Add a node to n to graph g.
  *
- * @param g Source graph
- * @param n Node to be added
- * @return Index of the node added on success or -1 on failure.
+ * New adjacency lists in and out are created for the node, after which the
+ * node is added into the nodes list in the graph.
+ *
+ * \param g Source graph
+ * \param n Node to be added
+ * \return Index of the node added on success or -1 on failure.
  ******************************************************************************/
 
 #include "../inc/graph.h"
