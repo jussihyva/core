548e94548ca02ae887c63ee59ed5f806e2987aa7
Satu Koskinen <satu.a.koskinen@gmail.com>
2021-05-07 21:47:59 +0300

    switch hashmap probing from quadratic to linear

--- inc/core.h
+++ inc/core.h
@@ -34,10 +34,10 @@
 # define CR_MAP_NULL_NODE (t_map_node) {NULL, NULL}
 # define CR_MAP_NULL (t_map) {NULL, 0, 0, 0.0, NULL, NULL, NULL}
 # define CR_MAP_START_CAPACITY 4
-# define CR_MAP_LOAD_FACTOR 0.4
+# define CR_MAP_LOAD_FACTOR 1
 # define CR_MAP_HASH map_hash_1
-# define CR_MAP_PROBE map_probe_quad_pow2
-# define CR_MAP_RESIZE map_resize_pow2
+# define CR_MAP_PROBE map_probe_linear
+# define CR_MAP_RESIZE map_resize_linear
 # define CR_PI 3.14159265358979323846
 # include <stdlib.h>
 # include <stdint.h>
---
+++ map/src/map_resize_linear.c
@@ -0,0 +1,7 @@
+#include "../inc/map.h"
+#include "../../inc/core.h"
+
+uint64_t	map_resize_linear(uint64_t capacity)
+{
+	return (capacity * 2);
+}
